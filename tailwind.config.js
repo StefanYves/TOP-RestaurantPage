5b9fdbc6de34bda40c4ae25ae5767f2362ce3503
StefanYves <stefannisipeanu02@gmail.com>
2023-12-02 20:44:39 +0200

    Resolved a bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ["./dist/*.html"],
+  content: [
+    "./dist/*.html",
+    "./src/**/*.js", // Include all JavaScript files in src directory and subdirectories
+    // Add other JavaScript files or directories as needed
+  ],
   theme: {
     extend: {
       backgroundImage: {
